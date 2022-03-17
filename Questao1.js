c95862c0b97d8d5ed8977fdef856f4614ce41de2
GabrielCecconi9 <99411346+GabrielCecconi9@users.noreply.github.com>
2022-03-17 12:34:21 -0300

    Add files via upload

---
+++ Questao1.js
@@ -0,0 +1,60 @@
+
+var nomealuno1 = "Caio Azevedo"
+var nomealuno2 = "Gabriel Keppk"
+var nomealuno3 = "Gabriel Cecconi"
+var nomealuno4 = "Leonardo João"
+var nomealuno5 = "Pedro Artur"
+ 
+console.log(nomealuno1)
+console.log(nomealuno2)
+console.log(nomealuno3)
+console.log(nomealuno4)
+console.log(nomealuno5)
+ 
+var inteiro1 = 10
+var inteiro2 = 20
+var inteiro3 = 30
+var inteiro4 = 40
+var inteiro5 = 50
+ 
+console.log(inteiro1)
+console.log(inteiro2)
+console.log(inteiro3)
+console.log(inteiro4)
+console.log(inteiro5)
+ 
+var correndo = true
+var pulando = false
+var descansando = false
+var comendo = true
+var voando = false
+ 
+console.log("Correndo " + correndo)
+console.log("Pulando " + pulando)
+console.log("Descansando " + descansando)
+console.log("Comendo " + comendo)
+console.log("Voando " + voando)
+ 
+let salariofun1 = 1000
+let salariofun2 = 2000
+let salariofun3 = 3000
+let salariofun4 = 4000
+let salariofun5 = 5000
+ 
+console.log ("O salario do funcionario 1 é " + salariofun1)
+console.log ("O salario do funcionario 2 é " + salariofun2)
+console.log ("O salario do funcionario 3 é " + salariofun3)
+console.log ("O salario do funcionario 4 é " + salariofun4)
+console.log ("O salario do funcionario 5 é " + salariofun5)
+ 
+const numeroint = 100
+const numeroreal = 200.2
+const numeroreal2 = 320.7
+const texto = " É O MOKA"
+const jogando = true
+ 
+console.log(numeroint)
+console.log(numeroreal)
+console.log(numeroreal2)
+console.log(texto)
+console.log("Jogando " + jogando)
